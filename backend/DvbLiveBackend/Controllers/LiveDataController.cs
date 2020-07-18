fd622a0b72ee00fa4f895377ff288eea48e42cdb
Sascha Bartl <bartl.sascha0@gmail.com>
2020-07-19 00:13:57 +0200

    Backend: Add Project

---
+++ backend/DvbLiveBackend/Controllers/LiveDataController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DerMistkaefer.DvbLive.Backend.Controllers
+{
+    /// <summary>
+    /// Controller with all routes for the live position of vehicles / lines
+    /// </summary>
+    [ApiController]
+    [Route("live")]
+    public class LiveDataController : ControllerBase
+    {
+        /// <summary>
+        /// initialisation with dependencies
+        /// </summary>
+        public LiveDataController()
+        {
+
+        }
+    }
+}
