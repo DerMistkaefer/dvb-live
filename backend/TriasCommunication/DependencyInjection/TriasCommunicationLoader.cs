2cfed6d5e627a41cc8dbf5a9ecd1274586f429b3
Sascha Bartl <bartl.sascha0@gmail.com>
2020-09-04 15:53:33 +0200

    Add Trias Communication - DependencyInjection

---
+++ backend/TriasCommunication/DependencyInjection/TriasCommunicationLoader.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace DerMistkaefer.DvbLive.TriasCommunication.DependencyInjection
+{
+    /// <summary>
+    /// Provides extension methods to add the TriasCommunication library to the .NET Core
+    /// dependency injection container.
+    /// </summary>
+    public static class TriasCommunicationLoader
+    {
+        /// <summary>
+        /// Adds the TriasCommunicator to the dependency injection container.
+        /// </summary>
+        /// <param name="services">Collection of services for building a dependency injection container.</param>
+        public static void AddTriasCommunication(this IServiceCollection services)
+        {
+            services.AddHttpClient();
+            services.AddSingleton<ITriasCommunicator, TriasCommunicator>();
+        }
+    }
+}
