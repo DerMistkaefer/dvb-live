a500a05c8e6bdb1497e1649805df009242cf753a
Sascha Bartl <bartl.sascha0@gmail.com>
2021-01-03 18:59:26 +0100

    Split IpStackResponse in seperated clases

--- backend/IPGeolocation/Models/IPStackResponse.cs
+++ backend/IPGeolocation/Models/IPStackResponse.cs
@@ -38,43 +38,4 @@ namespace DerMistkaefer.DvbLive.IPGeolocation.Models
         [JsonProperty(PropertyName = "location")]
         public IpStackLocationResponse Location { get; set; } = new();
     }
-
-    internal class IpStackLocationResponse
-    {
-        [JsonProperty(PropertyName = "geoname_id")]
-        public int GeonameId { get; set; }
-
-        [JsonProperty(PropertyName = "capital")]
-        public string Capital { get; set; } = "";
-
-        [JsonProperty(PropertyName = "languages")]
-        public IpStackLanguageResponse[] Languages { get; set; } = Array.Empty<IpStackLanguageResponse>();
-
-        [JsonProperty(PropertyName = "country_flag")]
-        public string CountryFlag { get; set; } = "";
-
-        [JsonProperty(PropertyName = "country_flag_emoji")]
-        public string CountryFlagEmoji { get; set; } = "";
-
-        [JsonProperty(PropertyName = "country_flag_emoji_unicode")]
-        public string CountryFlagEmojiUnicode { get; set; } = "";
-
-        [JsonProperty(PropertyName = "calling_code")]
-        public string CallingCode { get; set; } = "";
-
-        [JsonProperty(PropertyName = "is_eu")]
-        public bool IsEu { get; set; }
-    }
-
-    internal class IpStackLanguageResponse
-    {
-        [JsonProperty(PropertyName = "code")]
-        public string Code { get; set; } = "";
-
-        [JsonProperty(PropertyName = "name")]
-        public string Name { get; set; } = "";
-
-        [JsonProperty(PropertyName = "native")]
-        public string Native { get; set; } = "";
-    }
 }
---
+++ backend/IPGeolocation/Models/IpStackLocationResponse.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace DerMistkaefer.DvbLive.IPGeolocation.Models
+{
+    internal class IpStackLocationResponse
+    {
+        [JsonProperty(PropertyName = "geoname_id")]
+        public int GeonameId { get; set; }
+
+        [JsonProperty(PropertyName = "capital")]
+        public string Capital { get; set; } = "";
+
+        [JsonProperty(PropertyName = "languages")]
+        public IpStackLanguageResponse[] Languages { get; set; } = Array.Empty<IpStackLanguageResponse>();
+
+        [JsonProperty(PropertyName = "country_flag")]
+        public string CountryFlag { get; set; } = "";
+
+        [JsonProperty(PropertyName = "country_flag_emoji")]
+        public string CountryFlagEmoji { get; set; } = "";
+
+        [JsonProperty(PropertyName = "country_flag_emoji_unicode")]
+        public string CountryFlagEmojiUnicode { get; set; } = "";
+
+        [JsonProperty(PropertyName = "calling_code")]
+        public string CallingCode { get; set; } = "";
+
+        [JsonProperty(PropertyName = "is_eu")]
+        public bool IsEu { get; set; }
+    }
+}
